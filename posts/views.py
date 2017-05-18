058662c8aac56caf4c4f72c12073e217fbc22a0f
Jonatan Roytgars <jroytgarts@gmail.com>
2017-05-18 16:44:53 -0400

    uploading files

---
+++ posts/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from django.contrib.auth.models import User
+from .models import Post
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+@login_required
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['url']:
+            post = Post()
+            post.title = request.POST['title']
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                post.url = request.POST['url']
+            else:
+                post.url = 'http://' + request.POST['url']
+            post.pub_date = timezone.datetime.now()
+            post.author = request.user
+            post.save()
+            return redirect('home')
+        else:
+            return render(request, 'posts/create.html', {'error':'ERROR: You must include a title and a URL to create a post'})
+    else:
+        return render(request, 'posts/create.html')
+
+def home(request):
+    posts = Post.objects.order_by('-votes_total')
+    return render(request, 'posts/home.html', {'posts':posts})
+
+def upvote(request, pk):
+    if request.method == 'POST':
+        post = Post.objects.get(pk=pk)
+        post.votes_total += 1
+        post.save()
+        next = request.POST.get('next', '/')
+        return HttpResponseRedirect(next)
+        
+def downvote(request, pk):
+    if request.method == 'POST':
+        post = Post.objects.get(pk=pk)
+        post.votes_total -= 1
+        post.save()
+        next = request.POST.get('next', '/')
+        return HttpResponseRedirect(next)
+
+def userposts(request, fk):
+        posts = Post.objects.filter(author__id=fk).order_by('-votes_total')
+        author = User.objects.get(pk=fk)
+        return render(request, 'posts/userposts.html', {'posts':posts, 'author':author})
